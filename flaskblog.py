258aa42f8adffbc8a995c62e96620a1fd2962cb9
tugayjt <tugayjabbarov@gmail.com>
2024-07-11 01:53:57 +0400

    database with flask-sqlalchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,52 +1,96 @@
-from flask import Flask,render_template, url_for,flash,redirect
-from forms import RegistrationForm,LoginForm
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
+app.config["SECRET_KEY"] = "secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+
+with app.app_context():
+    db.create_all()
+
+
+class ModelMixin:
+    def save_to_db(self):
+        with app.app_context(): 
+            db.session.add(self)
+            db.session.commit()
+
+class User(db.Model, ModelMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username},{self.email},{self.image_file}')"
+
+
+class Post(db.Model, ModelMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+    def __repr__(self):
+        return f"Post('{self.title},{self.date_posted}')"
 
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20,2018'
+        "author": "Corey Schafer",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "April 20,2018",
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21,2018'
-    }
+        "author": "Jane Doe",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": "April 21,2018",
+    },
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts, title = 'home')
+    return render_template("home.html", posts=posts, title="home")
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template("about.html", title="About")
 
-@app.route("/register",methods = ['GET','POST'])
-def register(): 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
-        return redirect(url_for('home'))
-    return render_template('register.html',title='Register',form=form)
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
 
-@app.route("/login",methods = ['GET','POST'])
-def login(): 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!','success')
-            return redirect (url_for('home'))
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
         else:
-            flash('Login Unsuccessful.Please check username and password','danger')
-    return render_template('login.html',title='Login',form=form)
+            flash("Login Unsuccessful.Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
