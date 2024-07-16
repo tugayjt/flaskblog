bc823724c20e67649eb87ec0c5bf64b163e14e69
tugayjt <tugayjabbarov@gmail.com>
2024-07-16 18:19:56 +0400

    create, update, and delete posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,123 +1,62 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
-    """
-    Form for user registration.
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-    Attributes:
-        username (StringField): Username field.
-        email (StringField): Email field.
-        password (PasswordField): Password field.
-        confirm_password (PasswordField): Password confirmation field.
-        submit (SubmitField): Submit button.
-    """
-
-    username: StringField
-    email: StringField
-    password: PasswordField
-    confirm_password: PasswordField
-    submit: SubmitField
-
-    def validate_username(self, username: StringField):
-        """
-        Validate if the username is already taken.
-
-        Args:
-            username (StringField): Username to validate.
-
-        Raises:
-            ValidationError: If the username is already taken.
-        """
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError(
-                "That username is taken. Please choose a different one."
-            )
+            raise ValidationError('That username is taken. Please choose a different one.')
 
-    def validate_email(self, email: StringField):
-        """
-        Validate if the email address is already taken.
-
-        Args:
-            email (StringField): Email address to validate.
-
-        Raises:
-            ValidationError: If the email address is already taken.
-        """
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError("That email is taken. Please choose a different one.")
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
-    """
-    Form for user login.
-
-    Attributes:
-        email (StringField): Email field.
-        password (PasswordField): Password field.
-        remember (BooleanField): Checkbox to remember user session.
-        submit (SubmitField): Submit button.
-    """
-
-    email: StringField
-    password: PasswordField
-    remember: BooleanField
-    submit: SubmitField
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 
 class UpdateAccountForm(FlaskForm):
-    """
-    Form for updating user account details.
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
 
-    Attributes:
-        username (StringField): Username field.
-        email (StringField): Email field.
-        picture (FileField): Field for updating profile picture.
-        submit (SubmitField): Submit button.
-    """
-
-    username: StringField
-    email: StringField
-    picture: FileField
-    submit: SubmitField
-
-    def validate_username(self, username: StringField):
-        """
-        Validate if the new username is already taken.
-
-        Args:
-            username (StringField): New username to validate.
-
-        Raises:
-            ValidationError: If the new username is already taken.
-        """
+    def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError(
-                    "That username is taken. Please choose a different one."
-                )
+                raise ValidationError('That username is taken. Please choose a different one.')
 
-    def validate_email(self, email: StringField):
-        """
-        Validate if the new email address is already taken.
-
-        Args:
-            email (StringField): New email address to validate.
-
-        Raises:
-            ValidationError: If the new email address is already taken.
-        """
+    def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError(
-                    "That email is taken. Please choose a different one."
-                )
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flaskblog import db, app, login_manager
+from flaskblog import db, login_manager
 from flask_login import UserMixin
 
 
@@ -12,74 +12,29 @@ class ModelMixin:
             db.session.add(self)
             db.session.commit()
 
-
 @login_manager.user_loader
-def load_user(user_id: int) -> "User":
-    """
-    Load a user by ID.
-
-    Args:
-        user_id (int): The user ID.
-
-    Returns:
-        User: The user object.
-    """
+def load_user(user_id):
     return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
-    """
-    User model for storing user-related data.
-
-    Attributes:
-        id (int): The unique identifier for the user.
-        username (str): The username of the user.
-        email (str): The email address of the user.
-        image_file (str): The filename of the user's profile image.
-        password (str): The hashed password of the user.
-        posts (List[Post]): The list of posts authored by the user.
-    """
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship("Post", backref="author", lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
-    def __repr__(self) -> str:
-        """
-        Return a string representation of the user.
-
-        Returns:
-            str: The string representation of the user.
-        """
+    def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Post(db.Model):
-    """
-    Post model for storing post-related data.
-
-    Attributes:
-        id (int): The unique identifier for the post.
-        title (str): The title of the post.
-        date_posted (datetime): The date and time the post was created.
-        content (str): The content of the post.
-        user_id (int): The ID of the user who authored the post.
-    """
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self) -> str:
-        """
-        Return a string representation of the post.
-
-        Returns:
-            str: The string representation of the post.
-        """
+    def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
