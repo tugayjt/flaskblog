fd1613e43409f01cf0f82fd18a638b12586e0ba6
tugayjt <tugayjabbarov@gmail.com>
2024-07-19 00:19:34 +0400

    email and password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,50 +1,43 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
 
-# Create the Flask application instance
+# Load environment variables from a .env file
+load_dotenv()
+
+# Initialize Flask application
 app: Flask = Flask(__name__)
 
-# Configure the Flask application
+# Set secret key for the Flask application
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+
+# Configure SQLAlchemy with SQLite database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
-# Initialize extensions
+# Initialize SQLAlchemy
 db: SQLAlchemy = SQLAlchemy(app)
-bcrypt: Bcrypt = Bcrypt(app)
-login_manager: LoginManager = LoginManager(app)
 
-# Configure the LoginManager
+# Initialize Bcrypt for password hashing
+bcrypt: Bcrypt = Bcrypt(app)
+
+# Initialize LoginManager for managing user sessions
+login_manager: LoginManager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
-# Import routes
-import flaskblog.routes
+# Configure Flask-Mail with Gmail's SMTP server
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("app_mail")
+app.config["MAIL_PASSWORD"] = os.getenv("app_pass")
 
+# Initialize Mail
+mail: Mail = Mail(app)
 
-def create_app() -> Flask:
-    """
-    Create and configure the Flask application.
-
-    Returns:
-        Flask: The configured Flask application instance.
-    """
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-
-    # Initialize extensions
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-
-    # Import and register routes
-    with app.app_context():
-        import flaskblog.routes
-
-    return app
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# Import routes from the flaskblog package
+from flaskblog import routes
